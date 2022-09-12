7f87a4a62a6769046e82468651263069b261e08a
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2022-09-11 23:07:58 -0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        "very-dark-cyan": "hsl(192, 100%, 9%)",
+        "very-pale-blue": "hsl(207, 100%, 98%)",
+      },
+      colors: {
+        "primary-pink": "hsl(322, 100%, 66%)",
+        "light-pink": "hsl(321, 100%, 78%)",
+        "light-red": "hsl(0, 100%, 63%)",
+        "text-grey": "#7D8E9B",
+      },
+      fontFamily: {
+        openSans: ["Open Sans", "sans-serif"],
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
