4bfc262b06cd62da5573567b257fd0271c0ec949
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2022-09-15 18:04:07 -0700

    Add scroll reveal

---
+++ js/app.js
@@ -0,0 +1,4 @@
+ScrollReveal().reveal(".reveal-200", { delay: 200 });
+ScrollReveal().reveal(".reveal-250", { delay: 250 });
+ScrollReveal().reveal(".reveal-450", { delay: 450 });
+ScrollReveal().reveal(".reveal-750", { delay: 750 });
