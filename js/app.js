b988379d14f281414cf795ccdf2fdf62bee7c755
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2022-09-15 18:47:13 -0700

    Update scroll reveal delays

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 ScrollReveal().reveal(".reveal-200", { delay: 200 });
 ScrollReveal().reveal(".reveal-250", { delay: 250 });
+ScrollReveal().reveal(".reveal-225", { delay: 225 });
 ScrollReveal().reveal(".reveal-450", { delay: 450 });
-ScrollReveal().reveal(".reveal-750", { delay: 750 });
+ScrollReveal().reveal(".reveal-725", { delay: 725 });
