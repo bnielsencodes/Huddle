6f29a3d0cf8487462eb015093b8df019e14e9584
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2022-09-15 21:05:32 -0700

    Update page animations

--- js/app.js
+++ js/app.js
@@ -1,5 +1,11 @@
-ScrollReveal().reveal(".reveal-200", { delay: 200 });
-ScrollReveal().reveal(".reveal-250", { delay: 250 });
-ScrollReveal().reveal(".reveal-225", { delay: 225 });
-ScrollReveal().reveal(".reveal-450", { delay: 450 });
-ScrollReveal().reveal(".reveal-725", { delay: 725 });
+ScrollReveal().reveal(".delay-200", { delay: 200 });
+ScrollReveal().reveal(".delay-225", { delay: 225 });
+ScrollReveal().reveal(".delay-250", { delay: 250 });
+ScrollReveal().reveal(".delay-450", { delay: 450 });
+ScrollReveal().reveal(".delay-700", { delay: 700 });
+ScrollReveal().reveal(".enter-top", { origin: "top" });
+ScrollReveal().reveal(".enter-right", { origin: "right" });
+ScrollReveal().reveal(".enter-left", { origin: "left" });
+ScrollReveal().reveal(".distance-350", { distance: "350px" });
+ScrollReveal().reveal(".duration-500", { duration: 1500 });
+ScrollReveal().reveal(".ease", { easing: "ease" });
